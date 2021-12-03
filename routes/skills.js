6a9792627467012026335c60c989d8c7f3d2ced6
Ernest Bruno <erndobruno@gmail.com>
2021-12-03 14:32:56 -0800

    Catch the route for individual skill.

--- routes/skills.js
+++ routes/skills.js
@@ -5,4 +5,7 @@ const skillsCtrl = require('../controllers/skills');
 /* GET skills listing. */
 router.get('/', skillsCtrl.index);
 
+/* GET one skill based on id */
+router.get('/:id', skillsCtrl.show);
+
 module.exports = router;
